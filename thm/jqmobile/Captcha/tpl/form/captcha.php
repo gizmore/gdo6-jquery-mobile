919353595e5b6733c79503db9c602fa45ca8f404
gizmore <gizmore@wechall.net>
2021-05-14 08:15:25 +0200

    6.10.3-r1125_fixpack_mett_and_bootstrap

--- thm/jqmobile/Captcha/tpl/form/captcha.php
+++ thm/jqmobile/Captcha/tpl/form/captcha.php
@@ -4,18 +4,6 @@ use GDO\Core\GDT_Template;
 ?>
 <div class="ui-field-contain gdt-container<?=$field->classError()?>">
   <label <?=$field->htmlForID()?>><?=$field->htmlIcon()?><?=$field->displayLabel()?></label>
-  <span>
-  <input
-   <?=$field->htmlID()?>
-   class="ib"
-   autocomplete="off"
-   type="text"
-   pattern="[a-zA-Z]{5}"
-   size="5"
-   required="required"
-   <?=$field->htmlFormName()?>
-   value="<?=$field->displayVar()?>" />
-   <?=GDT_Template::php('Captcha', 'form/captcha_inner.php', ['field' => $field])?>
-  </span>
+  <?=GDT_Template::php('Captcha', 'form/captcha_inner.php', ['field' => $field])?>
   <?=$field->htmlError()?>
 </div>

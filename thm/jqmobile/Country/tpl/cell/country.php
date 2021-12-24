4abb89a09469913895842e394841da19a3095836
gizmore <gizmore@wechall.net>
2021-12-24 04:53:11 +0100

    6.11.1-r6281

--- thm/jqmobile/Country/tpl/cell/country.php
+++ thm/jqmobile/Country/tpl/cell/country.php
@@ -2,30 +2,27 @@
 use GDO\Country\GDO_Country;
 /** @var $field \GDO\Country\GDT_Country **/
 $country = $field->gdo;
-
 if (!($country instanceof GDO_Country ))
 {
     $country = GDO_Country::getById($field->var);
 }
-
-
 ?>
 <?php if ($country instanceof GDO_Country) : ?>
 <img
  class="gdo-country"
- alt="<?= $country->displayName(); ?>"
- title="<?= $country->displayName(); ?>"
- src="GDO/Country/img/<?= $country->getIDFile(); ?>.png" />
+ alt="??"
+ title="<?=$country->displayName()?>"
+ src="GDO/Country/img/<?=$country->getID()?>.png" />
 <?php if ($field->withName) : ?>
-<span><?= $country->displayName(); ?></span>
+<span><?=$country->displayName()?></span>
 <?php endif; ?>
 <?php else : ?>
 <img
  class="gdo-country"
- title="<?= t('unknown_country'); ?>"
- alt="<?= t('unknown_country'); ?>"
- src="GDO/Country/img/zz.png" />
+ title="<?=t('unknown_country')?>"
+ alt="??"
+ src="GDO/Country/img/ZZ.png" />
 <?php if ($field->withName) : ?>
-<span><?= t('unknown_country'); ?></span>
+<span><?=t('unknown_country')?></span>
 <?php endif;?>
 <?php endif;?>
